6b163735f95aeeb74298738cc288563f3ec51f3b
Claudio Grasso <claudio.grasso@yahoo.com>
2023-07-13 13:14:54 -0300

    Agrego pantalla de T&C

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,287 @@
+// main.dart
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_exit_app/flutter_exit_app.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:url_launcher/url_launcher.dart';
+// import 'package:form_builder_validators/form_builder_validators.dart';
+// import 'package:intl/intl.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+// https://phrase.com/blog/posts/flutter-localization/
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-void main() {
+const TextStyle _textStylePobre = TextStyle(
+  fontWeight: FontWeight.bold,
+  color: CupertinoColors.destructiveRed,
+);
+const TextStyle _textStyleInterm = TextStyle(
+  fontWeight: FontWeight.bold,
+  color: CupertinoColors.activeOrange,
+);
+const TextStyle _textStyleIdeal = TextStyle(
+  fontWeight: FontWeight.bold,
+  color: CupertinoColors.activeGreen,
+);
+
+SharedPreferences? prefs;
+bool? flagShowTandC = true;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+
+  // Obtain shared preferences.
+  prefs = await SharedPreferences.getInstance();
+
+  flagShowTandC = prefs!.getBool('showTandC');
+
+  debugPrint("Show T&C: $flagShowTandC");
+  // If I could not read it I assume T&C have not veen already accepted
+  flagShowTandC ??= true;
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return const CupertinoApp(
+      theme: CupertinoThemeData(
+        textTheme: CupertinoTextThemeData(
+          textStyle: TextStyle(fontSize: 18.0),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // Remove the debug banner
+      debugShowCheckedModeBanner: false,
+      title: 'Score',
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('en', ''), // English
+        // Locale('es', ''), // Spanish
+      ],
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  MyHomePageState createState() => MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class MyHomePageState extends State<MyHomePage> {
+  String? _selectedValueFuma;
+  String? _selectedValueBMI;
+  String? _selectedValueActiFis;
+  String? _selectedValueDieta;
+  String? _selectedValueColesterol;
+  String? _selectedValuePresion;
+  String? _selectedValueGlucemia;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  double _puntaje = 0.0;
+  String _score = '';
+  Color _colorScore = CupertinoColors.black;
+  bool _scoreVisible = false;
+
+  late BuildContext bc;
+  bool tcAccepted = false;
+
+  _computeScore() {
+    debugPrint('***************** Calculando el score *****************');
+
+    if (_selectedValueFuma != null &&
+        _selectedValueBMI != null &&
+        _selectedValueActiFis != null &&
+        _selectedValueDieta != null &&
+        _selectedValueColesterol != null &&
+        _selectedValuePresion != null &&
+        _selectedValueGlucemia != null) {
+      _puntaje = (double.parse(_selectedValueFuma!) +
+          double.parse(_selectedValueBMI!) +
+          double.parse(_selectedValueActiFis!) +
+          double.parse(_selectedValueDieta!) +
+          double.parse(_selectedValueColesterol!) +
+          double.parse(_selectedValuePresion!) +
+          double.parse(_selectedValueGlucemia!)) /
+          7;
+    }
+    debugPrint('Puntaje: $_puntaje');
+  }
+
+  showHint(BuildContext context, String title, String body) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(
+            title,
+            style: const TextStyle(fontSize: 16.0),
+          ),
+          content: SizedBox(
+              width: 400,
+              height: 400,
+              child: SingleChildScrollView(
+                child: Text(
+                  body,
+                  style: const TextStyle(fontSize: 14.0),
+                ),
+              )),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  showTandC(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(
+            AppLocalizations.of(context)!.txtImportantNotice,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+                color: Colors.deepOrange,
+                fontSize: 14.0,
+                fontWeight: FontWeight.bold),
+          ),
+          content: SizedBox(
+              width: 400,
+              height: 400,
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      AppLocalizations.of(context)!.txtReadCarefully,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      AppLocalizations.of(context)!.txtAppPurpose,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    GestureDetector(
+                      onTap: () async {
+                        await launchUrl(Uri.parse('https://www.heart.org'));
+                      },
+                      child: const Text(
+                        'American Heart Association',
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      AppLocalizations.of(context)!.txtFundamentalsLocation,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    GestureDetector(
+                      onTap: () async {
+                        await launchUrl(Uri.parse('https://playbook.heart.org/lifes-simple-7/'));
+                      },
+                      child: Text(
+                        AppLocalizations.of(context)!.txtImportantNotice,
+                        style: const TextStyle(
+                          decoration: TextDecoration.underline,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )),
+          actions: [
+            TextButton(
+              onPressed: () {
+                debugPrint("Cancelando...");
+                // Gracefully quit the app
+                // Según leí, Apple no lo permite pero esto, de alguna manera, lo logra.
+                // FlutterExitApp.exitApp(iosForceExit: true);
+                FlutterExitApp.exitApp();
+              },
+              child: const Text('Reject'),
+            ),
+            TextButton(
+              onPressed: () {
+                debugPrint("Aceptando...");
+                prefs!.setBool('showTandC', false);
+                Navigator.of(context).pop();
+              },
+              child: const Text('Accept'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  ////////////////////////////////////////////////////////////////
+  void myFunction() {
+    debugPrint('Widget built 1!');
+    showTandC(bc);
   }
 
   @override
+  void initState() {
+    super.initState();
+    debugPrint("En intiState $showTandC");
+
+    if (flagShowTandC != false) {
+      // Register a callback to execute after the widget is built
+      WidgetsBinding.instance.addPostFrameCallback((_) => myFunction());
+    }
+  }
+
+////////////////////////////////////////////////////////////////
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    final double screenWidth = MediaQuery.of(context).size.width;
+    // final double screenHeight = MediaQuery.of(context).size.height;
+
+    final gridColumnWidth = (screenWidth - 16.0) / 4.0;
+
+    debugPrint('Ancho de pantalla: ${screenWidth.toString()}');
+
+    // Acomodo el tamaño del font de acuerdo con el ancho de la pantalla
+    final fontSize = screenWidth / 75.0;
+
+    bc = context;
+
+    return DefaultTextStyle(
+      style: TextStyle(fontSize: fontSize),
+      child: Text("algo"),
     );
   }
 }
