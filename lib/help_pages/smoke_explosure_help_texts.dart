eb0e92072e8423f112645ddbc7157479a3ccc2a1
Claudio Grasso <claudio.grasso@yahoo.com>
2023-08-29 12:30:58 -0300

    Versión en Castellano del help de Smoke

--- lib/help_pages/smoke_explosure_help_texts.dart
+++ lib/help_pages/smoke_explosure_help_texts.dart
@@ -1,11 +1,168 @@
-const englishText1 =  r"""
-test
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////SPANISH TEXT ///////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+const spanishText =  r"""
+<b>Nicotine exposure</b></p>
+<p style="text-align: justify;">
+El consumo de tabaco es la principal causa de muerte prevenible en el mundo. 
+La mayoría de los fumadores diarios (82%) comienzan a fumar antes de los 18 años, 
+y más de 3,000 jóvenes comienzan a fumar cada día. Debido a que cuatro de cada cinco 
+personas que consumen tabaco comienzan antes de alcanzar la edad adulta, las actividades 
+de prevención del tabaco deben centrarse en los niños en edad escolar y adolescentes. 
+La evidencia sugiere que los programas de salud escolar pueden ser un medio efectivo para 
+prevenir el consumo de tabaco entre los jóvenes. Las pautas de los CDC ayudan al personal 
+escolar a planificar, implementar y evaluar programas educativos y políticas escolares 
+para prevenir el consumo de tabaco y la adicción, enfermedad y muerte innecesarias que causa. 
+Aunque estas pautas se centran en programas escolares desde el jardín de infantes hasta el 
+grado 12, las personas que trabajan con jóvenes en otros entornos también pueden encontrar 
+útiles las pautas. Los programas escolares diseñados para prevenir el consumo de tabaco 
+podrían convertirse en una de las estrategias más efectivas disponibles para reducir el 
+consumo de tabaco, con pautas que resumen las estrategias basadas en la escuela que son 
+efectivas para prevenir el consumo de tabaco entre los jóvenes.
+</p>
+<p style="text-align: justify;">
+Los cigarrillos electrónicos representan un cambio importante en el panorama del control 
+del tabaco, con un aumento dramático de su uso especialmente por parte de adolescentes y adultos 
+jóvenes, y están atrayendo a los jóvenes hacia nuevas vías de adicción a la nicotina. Aunque estos 
+productos pueden tener beneficios al ayudar a algunos fumadores a dejar de fumar o a pasar a un 
+producto menos dañino, los efectos a largo plazo en la salud de estos productos y el efecto neto 
+en la salud pública asociado con su uso siguen siendo inciertos y ampliamente debatidos. 
+Existe una preocupación cada vez mayor por que el uso de productos de tabaco más nuevos pueda 
+catalizar la transición al uso de otros productos de tabaco o drogas recreativas, especialmente 
+en adultos jóvenes. Es necesario monitorear el impacto de estas nuevas tecnologías en la salud de 
+la población, las enfermedades cardiovasculares y los accidentes cerebrovasculares, prestando especial 
+atención al efecto en los jóvenes y adolescentes. Es necesario continuar regulando de manera sólida 
+los productos de tabaco más nuevos, no solo para fortalecer los esfuerzos en curso de prevención y 
+control del tabaco, sino también para lograr una estrategia final para el mismo. También es necesario 
+realizar investigaciones independientes para evaluar el impacto del uso a largo plazo y del uso dual 
+en la salud de la población.</p>
+<p><b>Health
+Problemas causados por el humo de segunda mano (SHS)</b></p>
+<p style="text-align: justify;">
+Desde 1964, aproximadamente 2.500.000 personas que no fumaban murieron debido a problemas de salud causados
+por la exposición al humo de segunda mano. Los efectos del humo de segunda mano en el cuerpo son inmediatos, 
+con efectos inflamatorios y respiratorios perjudiciales en un plazo de 60 minutos después de la exposición, 
+que pueden durar al menos tres horas después de la misma. No hay un nivel seguro de exposición al humo de 
+segunda mano, incluso una exposición breve puede causar daño inmediato.</p>
+<p style="text-align: justify;">
+Salud
+Los problemas de salud causados por el SHS en adultos que no fuman incluyen enfermedad coronaria, accidente 
+cerebrovascular y cáncer de pulmón, así como efectos adversos en la salud reproductiva de las mujeres, 
+incluido el bajo peso al nacer. También puede resultar en muerte prematura.</p>
+<p style="text-align: justify;">
+El conjunto de pruebas epidemiológicas, observacionales y experimentales acumuladas hasta la fecha demuestra las consecuencias cardiovasculares perjudiciales de la exposición al SHS en los niños. Puede causar infecciones respiratorias, infecciones de oído y ataques de asma. En los bebés, puede causar el síndrome de muerte súbita del lactante (SMSL). Aunque los programas de salud pública han llevado a una disminución sustancial en la prevalencia del tabaquismo, los efectos adversos para la salud de la exposición al SHS de ninguna manera son cosa del pasado. Los niños son particularmente vulnerables, con poco control sobre el entorno doméstico y social, y carecen de comprensión, capacidad y habilidad para evitar la exposición al SHS por propia voluntad; también tienen características fisiológicas o comportamentales que los hacen especialmente susceptibles a los efectos. El humo de corriente lateral (el humo que emana del extremo encendido del cigarrillo), un componente importante del SHS, contiene una mayor concentración de algunas toxinas que el humo principal (inhalado directamente por el fumador), lo que lo hace potencialmente tan peligroso o incluso más peligroso que fumar directamente. Existe evidencia disponible sobre las consecuencias cardiovasculares para la salud de la exposición al SHS en la infancia, aumentando el riesgo de enfermedad cardiovascular en los niños. Los efectos de la exposición al SHS en el sistema cardiovascular en estudios pediátricos, incluyendo la disrupción vascular y la activación plaquetaria, la oxidación y la inflamación, la disfunción endotelial, el aumento de la rigidez vascular, los cambios en la estructura vascular y la disfunción autonómica, resultan en aterosclerosis prematura y sus consecuencias cardiovasculares. También afecta la función autonómica cardíaca y los cambios en la variabilidad de la frecuencia cardíaca, y está asociado con la agrupación de factores de riesgo cardiometabólico como la obesidad, la dislipidemia y la resistencia a la insulina. Las intervenciones individualizadas para reducir la exposición de los niños al SHS se ha demostrado que son al menos modestamente efectivas, al igual que las iniciativas políticas más amplias como las prohibiciones de fumar en la comunidad y el aumento de impuestos.</p>
+<p>Health problems caused by secondhand smoke:</p>
+<br/>
+<div align=center>
+<img src="asset:images/smoke_effect_children.png" alt="Children"  name="Image1" align="bottom" width="300" height="300" border="1"/>
+</br>
+<img src="asset:images/smoke_effect_adults.png" alt="Adults"  name="Image2" align="bottom" width="300" height="300" border="1"/>
+</div>
+&nbsp;</p>
+<p>&nbsp;</p>
+<p style="text-align: justify;">
+Aumentar la conciencia de las consecuencias cardiovasculares adversas y de por vida del SHS 
+en la infancia puede facilitar el desarrollo de intervenciones innovadoras centradas en el individuo, 
+la familia y la comunidad para reducir e idealmente eliminar la exposición al SHS en la población 
+pediátrica vulnerable. Esta evidencia requiere una sólida política de salud pública que abrace la 
+tolerancia cero a la exposición al SHS en la infancia.</p>
+<p style="text-align: justify;">
+<b>Exposición a la nicotina:</b>&nbsp;
+Los autores recomiendan restar 20 puntos a los niños y adultos expuestos al humo de segunda mano 
+en interiores en el hogar, dado su potencial para efectos a largo plazo en la salud cardiopulmonar. 
+El uso de sistemas de suministro de nicotina inhalada (por ejemplo, cigarrillos electrónicos o 
+dispositivos de vapeo) se ha añadido a la métrica anterior, que solo incluía el uso de cigarrillos 
+combustibles, para reflejar el uso de estos productos por parte de adultos y niños y sus implicaciones 
+para la salud a largo plazo. La exposición al humo de tabaco de segunda mano también se ha añadido a la 
+definición para reflejar su impacto adverso en la salud.
+
+Código de salida en un solo bloque 
+</p>
+
+  <ol>
+    <li style="margin-left: 1in; text-indent: -0.25in">Métrica de puntuación: Uso de tabaco combustible o inhalado</li>
+    <li style="margin-left: 1in; text-indent: -0.25in">NDS (sistema de suministro de nicotina) se puede usar a cualquier edad (según la discreción del clínico)</li>
+    <li style="margin-left: 1in; text-indent: -0.25in">NHANES, Encuestas Nacionales de Salud y Nutrición</li>
+    <li style="margin-left: 1in; text-indent: -0.25in"><b>Exposición al humo de segunda mano</b> se evalúa en todos los niños, y el puntaje es aplicable en cualquier edad pediátrica, incluyendo &lt;6 años de edad.</li>
+  </ol>
+
+<p></p>
+
+<table cellpadding="2" cellspacing="0" style="width:543px">
+	<tbody>
+		<tr>
+			<td style="background-color:#0066ff; border-color:#000000; border-style:solid; border-width:1pt; text-align:center; vertical-align:middle; width:80px">
+			<p style="text-align:center"><span style="font-size:10px"><span style="color:#ffffff"><strong>PUNTOS</strong></span></span></p>
+			</td>
+			<td style="background-color:#0066ff; border-color:#000000; border-style:solid; border-width:1pt; text-align:center; vertical-align:middle; width:447px">
+			<p style="text-align:center"><span style="font-size:10px"><span style="color:#ffffff"><strong>STATUS</strong></span></span></p>
+			</td>
+		</tr>
+		<tr>
+			<td style="border-bottom:1pt solid #000000; border-left:1pt solid #000000; border-right:1pt solid #000000; border-top:none; width:80px">
+			<p style="text-align:center"><span style="font-size:10px"><span style="font-family:Arial,Helvetica,sans-serif"><strong>100</strong></span></span></p>
+			</td>
+			<td style="border-bottom:1pt solid #000000; border-left:1pt solid #000000; border-right:1pt solid #000000; border-top:none; text-align:left; vertical-align:middle; width:447px">
+			<p><span style="font-size:10px"><span style="font-family:Arial,Helvetica,sans-serif">Nunca probó</span></span></p>
+			</td>
+		</tr>
+		<tr>
+			<td style="border-bottom:1pt solid #000000; border-left:1pt solid #000000; border-right:1pt solid #000000; border-top:none; width:80px">
+			<p style="text-align:center"><span style="font-size:10px"><span style="font-family:Arial,Helvetica,sans-serif"><strong>50</strong></span></span></p>
+			</td>
+			<td style="border-bottom:1pt solid #000000; border-left:1pt solid #000000; border-right:1pt solid #000000; border-top:none; text-align:left; vertical-align:middle; width:447px">
+			<p style="margin-right:0.12in"><span style="font-size:10px"><span style="font-family:Arial,Helvetica,sans-serif">Probó cualquier producto de nicotina, pero hace más de 30 días.</span></span></p>
+			</td>
+		</tr>
+		<tr>
+			<td style="border-bottom:1pt solid #000000; border-left:1pt solid #000000; border-right:1pt solid #000000; border-top:none; width:80px">
+			<p style="text-align:center"><span style="font-size:10px"><span style="font-family:Arial,Helvetica,sans-serif"><strong>25</strong></span></span></p>
+			</td>
+			<td style="border-bottom:1pt solid #000000; border-left:1pt solid #000000; border-right:1pt solid #000000; border-top:none; text-align:left; vertical-align:middle; width:447px">
+			<p><span style="font-size:10px"><span style="font-family:Arial,Helvetica,sans-serif">Actualmente utilizando NDS inhalado</span></span></p>
+			</td>
+		</tr>
+		<tr>
+			<td style="border-bottom:1pt solid #000000; border-left:1pt solid #000000; border-right:1pt solid #000000; border-top:none; width:80px">
+			<p style="text-align:center"><span style="font-size:10px"><span style="font-family:Arial,Helvetica,sans-serif"><strong>0</strong></span></span></p>
+			</td>
+			<td style="border-bottom:1pt solid #000000; border-left:1pt solid #000000; border-right:1pt solid #000000; border-top:none; text-align:left; vertical-align:middle; width:447px">
+			<p><span style="font-size:10px"><span style="font-family:Arial,Helvetica,sans-serif">Uso actual de combustibles (cualquier valor dentro de 30 d)</span></span></p>
+			</td>
+		</tr>
+		<tr>
+			<td colspan="2" style="border-bottom:1pt solid #000000; border-left:1pt solid #000000; border-right:1pt solid #000000; border-top:none; width:533px">
+			<p style="margin-right:0.06in"><span style="font-size:9px"><strong>Restar 20 puntos (a menos que la puntuación sea 0) por vivir con un fumador activo en el hogar.</strong></span></p>
+			</td>
+		</tr>
+	</tbody>
+</table>
+</center>
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+<p><strong>Bibliografía:</strong></p>
+
+<ol>
+	<li style="margin-left: 0.5in;">Raghuveer &nbsp;G, White DA, Hayman LL, Woo JG, Villafane J, Celermajer D, Ward KD, de Ferranti SD, Zachariah J; on behalf of the American Heart Association Committee on Atherosclerosis, &nbsp;Hypertension, &nbsp;and Obesity in the Young of the Council on Cardiovascular Disease in the Young; Behavior Change for Improving Health Factors Committee of the Council on Lifestyle and Cardiometabolic Health and Council on Epidemiology and Prevention; &nbsp;and Stroke Council. Cardiovascular consequences of childhood secondhand tobacco smoke exposure: prevailing evidence, burden, and racial and socioeconomic disparities: a scientific statement from the American Heart Association [published correction appears in Circulation. &nbsp;&nbsp;2016;134:e366]. &nbsp;Circulation. 2016;134:e33659. &nbsp;&nbsp;doi:10.1161/CIR.0000000000000443137. &nbsp;</li>
+	<li style="margin-left: 0.5in;">Bhatnagar A, Whitsel LP, &nbsp;Ribisl KM, Bullen C, &nbsp;Chaloupka F, &nbsp;Piano MR, Robertson &nbsp;RM, McAuley T, &nbsp;Goff D, Benowitz N; on behalf of the American Heart Association Advocacy Coordinating Committee, Council on Cardiovascular &nbsp;and Stroke Nursing, Council on Clinical Cardiology, and Council on Quality of Care and Outcomes Research. Electronic ciga- rettes: a policy statement from the American Heart Association. Circulation.2014;130:1418436. doi: 10.1161/CIR.0000000000000107</li>
+	<li style="margin-left: 0.5in;">Bhatnagar A, Whitsel LP, Blaha MJ, Huffman MD, Krishan-Sarin &nbsp;S, Maa J, Rigotti N, Robertson RM, Warner JJ. New and emerging tobacco prod- ucts and the nicotine endgame: the role of robust regulation and com- prehensive tobacco control and pre ntion: a presidential &nbsp;advisory from the American Heart Association. Circulation. 2019;139:e93758. doi:10.1161/CIR.0000000000000669</li>
+	<li style="margin-left: 0.5in;">Centers for Disease Control and Prevention, Office on Smoking and Health.Health effects of secondhand smoke. 2020. Accessed April 26,2022. &nbsp;&nbsp;<a href="https://www.cdc.gov/tobacco/data_statistics/fact_sheets/secondhand_smoke/health_effects/index.htm"><u>https://www.cdc.gov/tobacco/data_statistics/fact_sheets/secondhand_smoke/health_effects/index.htm</u></a></li>
+	<li style="margin-left: 0.5in;">National Health and Nutrition Examination Survey. Smoking and tobacco use: SMQ. 2015. Accessed February 15, 2022. <a href="https://wwwn.cdc.gov/nchs/data/nhanes/2015-2016/questionnaires/SMQ_I.pdf"><u>https://wwwn.cdc.gov/nchs/data/nhanes/2015-2016/questionnaires/SMQ_I.pdf</u></a></li>
+</ol>
+
+<p>&nbsp;</p>
+
 """;
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////ENGLISH TEXT ///////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const englishText =  r"""
 <b>Nicotine exposure</b></p>
-<p style="text-align: justify";>Tobacco use is the leading cause of 
+<p style="text-align: justify;">Tobacco use is the leading cause of 
 preventable death in the world. The
 majority of daily smokers (82%) began smoking before 18 years of age,
 and more than 3,000 young persons begin smoking each day. Because
@@ -24,7 +181,7 @@ effective strategies available to reduce tobacco use with guidelines
 that summarize school-based strategies to be effective in preventing
 tobacco use among youth.
 </p>
-<p style="text-align: justify";>E-cigarettes
+<p style="text-align: justify;">E-cigarettes
 represent a major change in the tobacco control landscape, with a
 dramatic rise in their use, especially by adolescents and young
 adults and are attracting youth to new avenues for nicotine
@@ -46,7 +203,7 @@ use and dual use on population health.
 </p>
 <p><b>Health
 Problems Caused by Secondhand Smoke (SHS)</b></p>
-<p style="text-align: justify";>Since
+<p style="text-align: justify;">Since
 1964, about 2,500,000 people who did not smoke died from health
 problems caused by secondhand smoke exposure. The effects of SHS on
 the body are immediate with harmful inflammatory and respiratory
@@ -54,12 +211,12 @@ effects within 60 minutes of exposure which can last for at least
 three hours after exposure. There is no safe level of exposure to
 secondhand smoke even brief exposure can cause immediate harm.
 </p>
-<p style="text-align: justify";>Health
+<p style="text-align: justify;">Health
 problems caused by SHS in adults who do not smoke include coronary
 heart disease, stroke, and lung cancer, as well as adverse
 reproductive health effects in women, including low birth weight. It
 can also result in premature death. &nbsp;&nbsp;</p>
-<p style="text-align: justify";>
+<p style="text-align: justify;">
 The epidemiological, observational, and experimental evidence accumulated
 to date demonstrates the detrimental cardiovascular consequences of
 SHS exposure in children. It can cause respiratory infections, ear
@@ -99,14 +256,14 @@ smoking bans and increased taxation.</p>
 </div>
 &nbsp;</p>
 <p>&nbsp;</p>
-<p style="text-align: justify";>Increased
+<p style="text-align: justify;">Increased
 awareness of the adverse, lifetime cardiovascular consequences of
 childhood SHS may facilitate the development of innovative
 individual, family-centered, and community health interventions to
 reduce and ideally eliminate SHS exposure in the vulnerable pediatric
 population. This evidence calls for a robust public health policy
 that embraces zero tolerance of childhood SHS exposure.</p>
-<p style="text-align: justify";><b>Nicotine exposure:</b>&nbsp;The
+<p style="text-align: justify;"><b>Nicotine exposure:</b>&nbsp;The
 writing group recommends subtracting 20 points for children and
 adults exposed to indoor secondhand smoke at home, given its
 potential for long-term effects on cardiopulmonary health. Use of
